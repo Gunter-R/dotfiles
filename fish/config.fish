2fecc5812b68187e7546384e1900ba9d29d4ac98
Gunter-R <2004vvr@gmail.com>
2024-12-20 08:43:34 +0300

    add command to remove from paths

--- fish/config.fish
+++ fish/config.fish
@@ -15,3 +15,12 @@ bind --user --mode insert \cj history-search-forward
 
 # past from a global clipboard
 bind --mode default --sets-mode insert p 'fish_clipboard_paste&&commandline -f repaint'
+
+
+# src: https://github.com/fish-shell/fish-shell/issues/8604
+function remove_path
+  if set -l index (contains -i "$argv" $fish_user_paths)
+    set -e fish_user_paths[$index]
+    echo "Removed $argv from the path"
+  end
+end
